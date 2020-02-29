1c8b6a8c87d02639d5f8a37870d0409614cfd088
pramilagm <pmiris.pramila@gmail.com>
2020-02-29 00:09:14 -0800

    deletion of component

--- dojo_assignments/Udemy_cours/React/react-app/src/App.js
+++ dojo_assignments/Udemy_cours/React/react-app/src/App.js
@@ -6,7 +6,11 @@ class App extends Component {
   state = {
     persons: [
       { name: "Pramila", age: 25 },
-      { name: "shirish", age: 28 }
+      { name: "shirish", age: 28 },
+      {
+        name: "Sita",
+        age: 54
+      }
     ],
     showPersons: false
   };
@@ -34,6 +38,11 @@ class App extends Component {
       showPersons: !doesShow
     });
   };
+  deletepersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
   render() {
     const style = {
       backgroundColor: "orange",
@@ -42,6 +51,36 @@ class App extends Component {
       padding: "8px",
       cursor: "pointer"
     };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletepersonHandler(index)}
+                changed={this.namechangeHandler}
+              ></Person>
+            );
+          })}
+          {/* <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            onClick={this.switnameHandler.bind(this, "Pramila Gharti")}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switnameHandler.bind(this, "Shirish Shrestha")}
+            changed={this.namechangeHandler}
+          >
+            I love games
+          </Person> */}
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hello React</h1>
@@ -49,23 +88,7 @@ class App extends Component {
         <button style={style} onClick={this.togglepersonHandler}>
           click to see changes
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              onClick={this.switnameHandler.bind(this, "Pramila Gharti")}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switnameHandler.bind(this, "Shirish Shrestha")}
-              changed={this.namechangeHandler}
-            >
-              I love games
-            </Person>
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
